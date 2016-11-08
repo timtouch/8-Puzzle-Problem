2bb957ce45d91a02909d0de23044cf7aa095c2b3
Timothy Touch <chaosbounder@gmail.com>
2016-11-07 22:53:27 -0800

    Refactor movement methods and add Node class

--- eightPuzzle.py
+++ eightPuzzle.py
@@ -1,45 +1,51 @@
 
 '''
-    For reference
+    For index reference
     [ 0 1 2
       3 4 5
       6 7 8 ]
 
 '''
 
+class Node:
+    def __init__(self, state, parent, operator, depth, cost):
+        self.state = state
+        self.parent = parent
+        self.operator = operator
+        self.depth = depth
+        self.cost = cost
 
 def eightPuzzle (initialState, finalState):
-    print("We are here ", initialState)
-    print("We want to get to here ", finalState)
+    displayBoard(initialState)
+    displayBoard(finalState)
 
-    print("Moved up ", moveUp(initialState))
-    print("Current State", initialState)
+    displayBoard(movement(initialState,'u'))
+    displayBoard(initialState)
+
+def displayBoard (boardState):
+    print(' {} {} {} \n {} {} {} \n {} {} {} \n'.format(boardState[0], boardState[1], boardState[2], boardState[3], boardState[4], boardState[5], boardState[6], boardState[7], boardState[8]))  
+
+'''
+    Takes in a board state and a direction and moves the empty space in that direction if possible.
+    The directions can be one of four states:
+       'u' - up
+       'd' - down
+       'l' - left
+       'r' - right
+'''
+def movement(state, direction):
+    emptySpace = state.index(0)
+    if direction == 'u' and emptySpace > 2:
+        state[emptySpace], state[emptySpace - 3] = state[emptySpace - 3], state[emptySpace]
+    if direction == 'd' and emptySpace < 6:
+        state[emptySpace], state[emptySpace + 3] = state[emptySpace + 3], state[emptySpace]
+    if direction == 'l' and mptySpace % 3 != 0:
+        state[emptySpace], state[emptySpace - 1] = state[emptySpace - 1], state[emptySpace]
+    if direction == 'r' and emptySpace % 3 != 2:
+        state[emptySpace], state[emptySpace + 1] = state[emptySpace + 1], state[emptySpace]
+    return state
 
 
-
-def moveUp(currentState):
-    emptySpace = currentState.index(0)
-    print("Location of empty space is ", emptySpace)
-    currentState[emptySpace], currentState[emptySpace - 3] = currentState[emptySpace - 3], currentState[emptySpace]
-    return currentState
-
-def moveDown(currentState):
-    emptySpace = currentState.index(0)
-    print("Location of empty space is ", emptySpace)
-    currentState[emptySpace], currentState[emptySpace + 3] = currentState[emptySpace + 3], currentState[emptySpace]
-    return currentState
-
-def moveLeft(currentState):
-    emptySpace = currentState.index(0)
-    print("Location of empty space is ", emptySpace)
-    currentState[emptySpace], currentState[emptySpace - 1] = currentState[emptySpace - 1], currentState[emptySpace]
-    return currentState
-
-def moveRight(currentState):
-    emptySpace = currentState.index(0)
-    print("Location of empty space is ", emptySpace)
-    currentState[emptySpace], currentState[emptySpace + 1] = currentState[emptySpace + 1], currentState[emptySpace]
-    return currentState
     
 
 initialState = [ 2, 8, 3,
@@ -49,7 +55,4 @@ initialState = [ 2, 8, 3,
 finalState = [ 1, 2, 3,
                8, 0, 4,
                7, 6, 5 ]
-
 eightPuzzle(initialState, finalState)
-
-print (moveUp(initialState))
